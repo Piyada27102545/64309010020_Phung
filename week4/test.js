e3177dd5a4b12790a06632e48a43edd66a515a09
pattama2802 <116327515+pattama2802@users.noreply.github.com>
2022-11-11 13:39:40 +0700

    Create test.js

---
+++ week4/test.js
@@ -0,0 +1,12 @@
+var myName ='Pattama';
+console.log(myName)
+
+let lastName = 'Komkam';
+const age = 20;
+//age = 26;
+
+var year = 2022;
+let grade=3.69;
+
+grade = Math.floor(grade)
+console.log(grade)
